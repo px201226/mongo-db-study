c5f933efe47f7c40476fd2f9a18aa651fb006408
Soo <px201226@gmail.com>
2023-06-21 19:01:24 +0900

    mongoDB test code 추가

--- src/test/java/com/example/mongodb/MongoDbCrudTest.java
+++ src/test/java/com/example/mongodb/MongoDbCrudTest.java
@@ -3,14 +3,24 @@ package com.example.mongodb;
 import com.mongodb.MongoCredential;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import java.util.ArrayList;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.util.Lists;
+import org.bson.Document;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 public class MongoDbCrudTest {
+
+	MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+	String dbName = "myDb";
+	MongoDatabase database = mongoClient.getDatabase(dbName);
 
 
 	@Test
@@ -18,19 +28,57 @@ public class MongoDbCrudTest {
 	void test1() {
 
 		// given
-		final var dbName = "myDb";
 		final var userName = "sampleUser";
 
 		// when
-		MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
-		MongoDatabase database = mongoClient.getDatabase(dbName);
-
 		final var credential = MongoCredential.createCredential(userName, dbName, "password".toCharArray());
 
 		// then
 		Assertions.assertEquals(userName, credential.getUserName());
 		Assertions.assertEquals(dbName, credential.getSource());
+
+
 	}
 
+	@Test
+	@DisplayName("Show DatabaseNames")
+	void test2() {
+
+		database.createCollection("sampleCollection");
+
+		// Inserting document into the collection
+		final MongoCollection<Document> collection = database.getCollection("sampleCollection");
+		Document document = new Document("title", "MongoDB")
+				.append("description", "database")
+				.append("likes", 100)
+				.append("by", "tutorials point");
+
+
+		collection.insertOne(document);
+
+		// retrieve databaseNames
+		for (final String name : mongoClient.listDatabaseNames()) {
+			log.info(name);
+		}
+
+	}
+
+	@Test
+	@DisplayName("Show CollectionNames")
+	void test3() {
+
+		// before
+		final var sampleCollection2 = database.getCollection("sampleCollection2");
+		sampleCollection2.drop();
+
+		// given
+		database.createCollection("sampleCollection2");
+
+		// retrieve databaseNames
+		final var databaseNames = Lists.newArrayList(database.listCollectionNames());
+
+		// then
+		Assertions.assertTrue(databaseNames.contains("sampleCollection2"));
+	}
 
 }
